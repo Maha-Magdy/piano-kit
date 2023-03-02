027d2691d1f34b0e493afa5fc3ec5d5085a1f510
maha-magdy <maha.magdy@reviewsolicitors.com>
2023-03-02 08:43:44 +0000

    Handle the event for each keydown to remove the play class after the transition end

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-window.addEventListener('keydown', (e) => {
+const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('play');
-});
+}
 
-// keys = document.querySelectorAll('.piano-ele');
-// keys.forEach(key => addEventListener('transitionend', removeTransitionClass));
+window.addEventListener('keydown', playSound);
+
+const removeTransitionClass = (e) => {
+    if (e.propertyName !== 'box-shadow') return;
+    console.log(e.target);
+    e.target.classList.remove('play');
+}
+
+keys = document.querySelectorAll('.piano-ele');
+keys.forEach(key => addEventListener('transitionend', removeTransitionClass));
